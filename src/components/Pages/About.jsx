9be71e6d2ea0c25100564775b31c3a817987ad40
Arpit <menariaarpit2@gmail.com>
2025-06-02 15:53:59 +0530

    Initial commit

--- src/components/Pages/About.jsx
+++ src/components/Pages/About.jsx
@@ -1,50 +1,81 @@
-import React from 'react'
-import './About.css'
+import React from 'react';
+import './About.css';
 
 export const About = () => {
   return (
-    <div>
-        <h3>
-            About
-         </h3>
-    <div>
-    <p>
-    <h4>Our Story</h4>
-    <h3>About our Store</h3>
-    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum, harum. Consequuntur molestiae, expedita accusantium odit libero dolores aspernatur, <br />est nemo illum fugiat, id vel quam aut voluptates magni nostrum repellendus.
-    </p>
-     </div>
+    <div className="about-container">
+      <h3>About</h3>
 
-     <div>
+      <div className="section">
+        <h4>Our Story</h4>
+        <h3>About our Store</h3>
         <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum, harum.
+          Consequuntur molestiae, expedita accusantium odit libero dolores aspernatur,
+          est nemo illum fugiat, id vel quam aut voluptates magni nostrum repellendus.
+        </p>
+      </div>
+
+      <div className="section">
         <h3>Our Vision</h3>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. <br /> Pariatur, suscipit omnis. Exercitationem, nemo ex quae aspernatur <br />laborum quaerat neque sapiente.
-        </p>
         <p>
-            <h3>Our Mission</h3>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. <br /> Pariatur, suscipit omnis. Exercitationem, nemo ex quae aspernatur <br />laborum quaerat neque sapiente.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Pariatur, suscipit omnis. Exercitationem, nemo ex quae aspernatur
+          laborum quaerat neque sapiente.
         </p>
-        <p>
-            <h3>Our Goal</h3>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. <br /> Pariatur, suscipit omnis. Exercitationem, nemo ex quae aspernatur <br />laborum quaerat neque sapiente.
-        </p>
-     </div>
 
-     <div>
+        <h3>Our Mission</h3>
         <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Pariatur, suscipit omnis. Exercitationem, nemo ex quae aspernatur
+          laborum quaerat neque sapiente.
+        </p>
+
+        <h3>Our Goal</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Pariatur, suscipit omnis. Exercitationem, nemo ex quae aspernatur
+          laborum quaerat neque sapiente.
+        </p>
+      </div>
+
+      <div className="section">
         <h2>Team Members</h2>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. <br />Repellendus iure dolorum incidunt placeat facilis quidem!
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Repellendus iure dolorum incidunt placeat facilis quidem!
         </p>
-     </div>
+      </div>
 
-     <div>
+      <div className="section">
         <h3>What our clients say?</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates assumenda <br />adipisci voluptatum cum iusto. Ipsam nemo fuga doloremque veritatis tempora. <h4>Nancy<p>Director at xdesign</p></h4></p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates assumenda <br />adipisci voluptatum cum iusto. Ipsam nemo fuga doloremque veritatis tempora. <h4>Sameul<p>Director at xdesign</p></h4></p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates assumenda <br />adipisci voluptatum cum iusto. Ipsam nemo fuga doloremque veritatis tempora. <h4>Daniel<p>Director at xdesign</p></h4></p>
+
+        <div className="testimonial">
+          <p>
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Voluptates assumenda adipisci voluptatum cum iusto. Ipsam nemo fuga doloremque veritatis tempora."
+          </p>
+          <h4>Nancy <p>Director at XDesign</p></h4>
         </div>
 
+        <div className="testimonial">
+          <p>
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Voluptates assumenda adipisci voluptatum cum iusto. Ipsam nemo fuga doloremque veritatis tempora."
+          </p>
+          <h4>Samuel <p>Director at XDesign</p></h4>
+        </div>
+
+        <div className="testimonial">
+          <p>
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Voluptates assumenda adipisci voluptatum cum iusto. Ipsam nemo fuga doloremque veritatis tempora."
+          </p>
+          <h4>Daniel <p>Director at XDesign</p></h4>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
+
 export default About;
